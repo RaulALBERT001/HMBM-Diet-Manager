12d9bda6d59e64437ffd3733f4ae2cdfa3f5aa4d
RaulABERT001 <albertiraulsergio@gmail.com>
2025-04-17 16:16:01 -0300

    #BTR01:feat/initial commit , setting up the project

---
+++ src/main/java/com/M/N0/HMBM/Diet/HmbmDietManagerApplication.java
@@ -0,0 +1,13 @@
+package com.M.N0.HMBM.Diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmbmDietManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmbmDietManagerApplication.class, args);
+	}
+
+}
